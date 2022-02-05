2f3ab48ab2a024deeb26c0a51052d554897ec6fa
Collin-5 <collins380@gmail.com>
2022-02-06 00:40:41 +0300

    states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+"""import storage engine and classes"""
+from models import storage
+from models.state import State
+
 """import flask views"""
 from api.v1.views.index import *
+from api.v1.views.states import *
