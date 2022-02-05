2b755c509a5ec7d9c69765f5e7ed41bf1cbb1df0
Collin-5 <collins380@gmail.com>
2022-02-05 23:35:38 +0300

    status

---
+++ api/v1/views/index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+    flask with general routes
+    routes:
+        /status:    display "status":"OK"
+        /stats:     dispaly total for all classes
+'''
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route("/status")
+def status():
+    '''
+        return JSON of OK status
+    '''
+    return jsonify({'status': 'OK'})
