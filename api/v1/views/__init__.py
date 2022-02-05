2a39bd64265b619c90e140ed7c191530f6a61a8e
Collin-5 <collins380@gmail.com>
2022-02-06 00:51:58 +0300

    cities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,7 +4,9 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 """import storage engine and classes"""
 from models import storage
 from models.state import State
+from models.city import City
 
 """import flask views"""
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
